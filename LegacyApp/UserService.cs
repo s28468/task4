5e8dd3a6d310cb4d57c995a83a5c52d539192279
Kyrylo Turchyn <kirulltruchin@gmail.com>
2024-04-01 15:42:26 +0200

    Initial commit

---
+++ LegacyApp/UserService.cs
@@ -0,0 +1,82 @@
+﻿using System;
+
+namespace LegacyApp
+{
+    public class UserService
+    {
+        public bool AddUser(string firstName, string lastName, string email, DateTime dateOfBirth, int clientId)
+        {
+            if (!ValidateUserData(firstName, lastName, email, dateOfBirth))
+            {
+                return false;
+            }
+
+            var clientRepository = new ClientRepository();
+            var client = clientRepository.GetById(clientId);
+
+            var user = CreateUser(firstName, lastName, email, dateOfBirth, client);
+
+            AssignCreditLimit(client, user);
+
+            if (user.HasCreditLimit && user.CreditLimit < 500)
+            {
+                return false;
+            }
+
+            UserDataAccess.AddUser(user);
+            return true;
+        }
+        private static bool ValidateUserData(string firstName, string lastName, string email, DateTime dateOfBirth)
+        {
+            return !string.IsNullOrEmpty(firstName) &&
+                   !string.IsNullOrEmpty(lastName) &&
+                   email.Contains("@") &&
+                   email.Contains(".") &&
+                   CalculateAge(dateOfBirth) >= 21;
+        }
+        private static int CalculateAge(DateTime dateOfBirth)
+        {
+            var now = DateTime.Now;
+            int age = now.Year - dateOfBirth.Year;
+            if (now.Month < dateOfBirth.Month || (now.Month == dateOfBirth.Month && now.Day < dateOfBirth.Day))
+                age--;
+            return age;
+        }
+        private static User CreateUser(string firstName, string lastName, string email, DateTime dateOfBirth, Client client)
+        {
+            return new User
+            {
+                Client = client,
+                DateOfBirth = dateOfBirth,
+                EmailAddress = email,
+                FirstName = firstName,
+                LastName = lastName
+            };
+        }
+        private void AssignCreditLimit(Client client, User user)
+        {
+            if (client.Type == "VeryImportantClient")
+            {
+                user.HasCreditLimit = false;
+            }
+            else if (client.Type == "ImportantClient")
+            {
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    creditLimit = creditLimit * 2;
+                    user.CreditLimit = creditLimit;
+                }
+            }
+            else
+            {
+                user.HasCreditLimit = true;
+                using (var userCreditService = new UserCreditService())
+                {
+                    int creditLimit = userCreditService.GetCreditLimit(user.LastName, user.DateOfBirth);
+                    user.CreditLimit = creditLimit;
+                }
+            }
+        }
+    }
+}
